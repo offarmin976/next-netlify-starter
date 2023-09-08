e1752ff8ee9f2a1393b42f450efabf8f06b39d1c
offarmin976 <armin.bajrica@me.com>
2023-09-08 14:40:45 +0200

    pci2

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="container">
       <Head>
         <title>Next.js Starter!</title>
-        <link rel="icon" href="/195619.jpg.png" />
+        <link rel="icon" href="/animated.png" />
       </Head>
 
       <main>
