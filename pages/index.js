daf3cf496394f95d5207bf9ef0d9371e6dd10674
offarmin976 <armin.bajrica@me.com>
2023-09-08 14:36:24 +0200

    Picture Edit

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="container">
       <Head>
         <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/195619.jpg.png" />
       </Head>
 
       <main>
