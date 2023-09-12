ba1e806adb4a5155faf80200c4c997612f21ae51
offarmin976 <armin.bajrica@me.com>
2023-09-13 00:36:33 +0200

    favicon change

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="container">
       <Head>
         <title>Next.js Starter!</title>
-        <link rel="icon" href="/animated.png" />
+        <link rel="icon" href="/CoverIG.png" />
       </Head>
 
       <main>
