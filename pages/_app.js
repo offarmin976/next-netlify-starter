ea4ca93c68572ef74bcef5a9ef68abab3079eb03
offarmin976 <armin.bajrica@me.com>
2023-09-14 23:59:06 +0200

    lllk

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,8 @@
 import '@styles/globals.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import 'public/bootstrap.min.css';
+import 'public/bootstrap.min.js';
 import 'jquery/dist/jquery.min.js';
 import 'popper.js/dist/umd/popper.min.js';
-import 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js'
 
 
 function Application({ Component, pageProps }) {
