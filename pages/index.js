a37d0c3755b484d24dfc6f6f5fdd7b565b2e5004
offarmin976 <armin.bajrica@me.com>
2023-09-14 22:35:17 +0200

    push loader

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,47 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import { useEffect } from 'react';
 
 export default function Home() {
+  // Funktion zum Erzeugen eines Pixel-Fehlers
+  function createPixelError() {
+    const pixelError = document.createElement("div");
+    pixelError.style.width = "1px";
+    pixelError.style.height = "1px";
+    pixelError.style.backgroundColor = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+    pixelError.style.position = "absolute";
+    pixelError.style.left = `${Math.random() * window.innerWidth}px`;
+    pixelError.style.top = `${Math.random() * window.innerHeight}px`;
+    document.body.appendChild(pixelError);
+  }
+
+  // Pixel-Fehler erstellen, wenn die Komponente geladen wird
+  useEffect(() => {
+    const numErrors = 1000; // Anzahl der simulierten Pixel-Fehler
+    for (let i = 0; i < numErrors; i++) {
+      createPixelError();
+    }
+  }, []);
+
   return (
-    <div class="loader"></div>
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/CoverIG.png" />
-      </Head>
+    <>
+      <div className="loader"></div>
+      <div className="container">
+        <Head>
+          <title>Next.js Starter!</title>
+          <link rel="icon" href="/CoverIG.png" />
+        </Head>
 
-      <main>
-        <Header title="XBLADE2 - DYNAMIC THREATS" />
-        <p className="19/10/23">
-        <button onclick="location.href = 'www.xblade2.bandcamp.com';" id="myButton" class="float-left submit-button" >LISTEN HERE</button>
-        </p>
-      </main>
+        <main>
+          <Header title="XBLADE2 - DYNAMIC THREATS" />
+          <p className="19/10/23">
+            <button onClick={() => window.location.href = 'https://www.xblade2.bandcamp.com'} id="myButton" className="float-left submit-button">LISTEN HERE</button>
+          </p>
+        </main>
 
-      <Footer />
-    </div>
+        <Footer />
+      </div>
+    </>
   )
 }
