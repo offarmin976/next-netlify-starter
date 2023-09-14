f332f5d8061121d9d1b377d0c4d57f7a35fed2c5
offarmin976 <armin.bajrica@me.com>
2023-09-14 23:18:29 +0200

    s9

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import '@styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import 'jquery/dist/jquery.min.js';
+import 'popper.js/dist/umd/popper.min.js';
 
 
 function Application({ Component, pageProps }) {
